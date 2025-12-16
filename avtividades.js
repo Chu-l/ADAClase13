c91f87eb5d1fbf756ea897e8ea9a4af842d2588e
Lilen <lilen.chu@hotmail.com>
2025-12-16 01:17:54 -0300

    ejercicio 6 terminado

--- avtividades.js
+++ avtividades.js
@@ -45,8 +45,17 @@ console.log(esPar(258)) // Prueba de la función
 /*** Ejercicio 5: Concatenar nombres (función expresada)
 Crea una función expresada llamada concatenarNombres que reciba dos nombres (nombre y apellido) como parámetros y devuelva el nombre completo concatenado.
 ***/
-
+/*
 let concatenarNombres = function(nombre,apellido) {
     return nombre + " " + apellido;
 };
 console.log(concatenarNombres("Susana","Giménez"));
+*/
+/*** Ejercicio 6: Convertir a minutos
+Crea una función declarada llamada convertirHorasAMinutos que reciba un número de horas como parámetro y devuelva el total en minutos.
+***/
+
+function convertirHorasAMinutos(numero) {
+    return numero*60;
+};
+console.log(convertirHorasAMinutos(8));
