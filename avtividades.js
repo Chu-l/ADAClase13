ae930633dd424e115b94e78a152d0208caa04385
Lilen <lilen.chu@hotmail.com>
2025-12-16 18:17:38 -0300

    Ejercicio 7 terminado

--- avtividades.js
+++ avtividades.js
@@ -54,8 +54,24 @@ console.log(concatenarNombres("Susana","Giménez"));
 /*** Ejercicio 6: Convertir a minutos
 Crea una función declarada llamada convertirHorasAMinutos que reciba un número de horas como parámetro y devuelva el total en minutos.
 ***/
-
+/*
 function convertirHorasAMinutos(numero) {
     return numero*60;
 };
 console.log(convertirHorasAMinutos(8));
+*/
+/*** Ejercicio 7: Mensaje según la hora
+Declara una variable global llamada horaActual (puedes asignarle un valor fijo). Crea una función declarada llamada mostrarSaludo que imprima "Buenos días" si la hora es menor a 12, "Buenas tardes" si es menor a 18, o "Buenas noches" en cualquier otro caso.
+***/
+
+let horaActual = 19;
+function mostrarSaludo() {
+    if (horaActual < 12) {
+        console.log("Buenos días");
+    } else if (horaActual < 18) {
+        console.log("Buenas tardes");
+    } else {
+        console.log("Buenas noches");
+    }
+};
+mostrarSaludo();
